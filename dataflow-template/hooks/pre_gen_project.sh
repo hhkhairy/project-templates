5cb3408ad375cc7e45716aaec75bf40be0499b1b
Hazem Khairy <hkhairy@adam.ai>
2023-05-10 22:49:08 +0300

    Add a pregen bash script to handle pyenv and poetry

---
+++ dataflow-template/hooks/pre_gen_project.sh
@@ -0,0 +1,17 @@
+# Create the pyenv venv and activate it in cookiecutter
+pyenv virtualenv {{ cookiecutter.python_version }} {{ cookiecutter.venv_name }}
+pyenv local {{ cookiecutter.venv_name }}
+
+## Poetry
+# For safety, configure poetry to respect the pyenv venv
+poetry config virtualenvs.in-project false --local
+poetry config virtualenvs.prefer-active-python true --local
+poetry config virtualenvs.create false --local
+# init poetry
+poetry init
+# add typical dependencies for dev and test
+poetry add pytest --group test
+poetry add ruff --group test
+poetry add jupyter --group dev
+
+
