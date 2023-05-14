eb3153811e74284f06afbb88e74bc76c00a66b63
Hazem Khairy <hkhairy@adam.ai>
2023-05-14 12:37:54 +0300

    Multiple templates

---
+++ dataflow-template/hooks/post_gen_project.py
@@ -0,0 +1,11 @@
+import os
+
+if __name__ == "__main__":
+    os.system("poetry config virtualenvs.in-project true --local")
+    os.system("poetry config virtualenvs.create true --local")
+    os.system("poetry config virtualenvs.prefer-active-python true --local")
+    
+    # TODO: We can expand this to add the latest version of testing dependencies instead of 
+    # hardcoding them in the pyproject.toml file. 
+    # TODO: We can also create another version to use pip with extras
+    os.system("poetry install")
